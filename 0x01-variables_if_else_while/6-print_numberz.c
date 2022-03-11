ae667f53d4e0d59fb664eefdef543787cf0e5bd3
josiashod <djossoujosiashod@gmail.com>
2022-03-11 14:01:12 +0100

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '1'; n <= '9'; n++)
+		n = putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
