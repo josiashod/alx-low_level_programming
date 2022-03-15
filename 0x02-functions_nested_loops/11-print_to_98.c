31ad1b908af8848ef991bd9a210a0c41144f2143
josiashod <djossoujosiashod@gmail.com>
2022-03-15 17:54:10 +0100

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers
+ * from n to 98, followed by a new line.
+ *
+ * @n: number to start printing from
+ *
+ * Return: void.
+ */
+void print_to_98(int n)
+{
+	int breakpoint = 98;
+
+	if (n > 98)
+	{
+		breakpoint = n;
+		n = 98;
+	}
+
+	for (n; n < breakpoint; n++)
+	{
+		printf("%d, ", n);
+	}
+
+	printf("%d\n", breakpoint);
+}
