29bc8da5dfeb5ccede2694a7fbaa0d302509d9f7
josiashod <djossoujosiashod@gmail.com>
2022-03-15 18:01:23 +0100

    Task 11 (update)

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,13 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		breakpoint = n;
-		n = 98;
+		for (; n > 98; n--)
+			printf("%d, ", n);
 	}
-
-	for (; n < breakpoint; n++)
+	else
 	{
-		printf("%d, ", n);
+		for (; n < 98; n++)
+			printf("%d, ", n);
 	}
 
 	printf("%d\n", breakpoint);
