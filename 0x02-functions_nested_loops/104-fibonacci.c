f76f549c71b70ee417161097796308923e09e77a
josiashod <djossoujosiashod@gmail.com>
2022-03-15 19:39:53 +0100

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,19 +9,43 @@
 int main(void)
 {
 	int i;
-	unsigned long int u0 = 0, u1 = 1, u;
+	unsigned long u0 = 0, u1 = 1, u, u_h1, u_h2;
+	unsigned long u0_h1, u1_h1, u0_h2, u1_h2;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 92; i++)
 	{
 		u = u0 + u1;
-		if (i < 97)
-			printf("%lu, ", u);
-		else
-			printf("%lu\n", u);
-
 		u0 = u1;
 		u1 = u;
+		printf("%lu, ", u);
 	}
 
+	u0_h1 = u0 / 10000000000;
+	u1_h1 = u0 / 10000000000;
+	u0_h2 = u0 % 10000000000;
+	u1_h2 = u0 % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		u_h1 = u0_h1 + u1_h1;
+		u_h2 = u0_h2 + u1_h2;
+
+		if (u_h2 > 9999999999)
+		{
+			u_h1++;
+			u_h2 %= 10000000000;
+		}
+
+		u0_h1 = u1_h1;
+		u0_h2 = u1_h2;
+		u1_h1 = u_h1;
+		u1_h2 = u_h2;
+
+		printf("%lu%lu", u_h1, u_h2);
+		if (i < 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
