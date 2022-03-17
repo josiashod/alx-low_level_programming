e40464dc26b5ee34aa2edb2c9e139dc09270be8c
josiashod <djossoujosiashod@gmail.com>
2022-03-17 11:25:51 +0100

    Task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+/**
+ * File: main.h
+ * Author: Josias DJOSSOU-DAN
+ *
+ * Description: Header file containing functions declaration
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+int _isupper(int c);
+
+#endif /* MAIN_H */
