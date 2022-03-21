2d852be80679415c9df306874d23bf296494b436
josiashod <djossoujosiashod@gmail.com>
2022-03-21 13:30:12 +0100

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value to 98.
+ * @n: pointer to the value to reset.
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+/**
+ * File: main.h
+ * Author: Josias DJOSSOU-DAN
+ *
+ * Description: Header file containing functions declaration
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
