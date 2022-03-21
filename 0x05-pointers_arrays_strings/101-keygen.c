d12192c04e06255ecfe907f4872a16a98290a251
josiashod <djossoujosiashod@gmail.com>
2022-03-21 18:59:05 +0100

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random password
+ *
+ * Return: Always success.
+ */
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
