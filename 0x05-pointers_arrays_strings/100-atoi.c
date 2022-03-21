f057185179e04c28a4de0f36fc9f3e1d2e37392a
josiashod <djossoujosiashod@gmail.com>
2022-03-21 18:43:46 +0100

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,12 +14,12 @@ int _atoi(char *s)
 	do
 	{
 		if (*s == '-')
-			i *= -1;
+			signe *= -1;
 		else if (*s >= '0' or *s <= '9')
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
 
-	return (num * i);
+	return (num * signe);
 }
