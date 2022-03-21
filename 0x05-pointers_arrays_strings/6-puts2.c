89b67174f1374c6858f9e89fd1c2938a9e01ee82
josiashod <djossoujosiashod@gmail.com>
2022-03-21 15:19:18 +0100

    Task 6(update)

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: string whose length should be return
+ *
+ * Return: (int) the length of the string.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
  * puts2 - prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: string whose length should be return
@@ -9,9 +27,9 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = _strlen(str);
 
-	while (str[i] != '\0')
+	while (i < len)
 	{
 		_putchar(str[i]);
 		i += 2;
