95102838567dc60eb4f5cd3eb7d949de1222d0ce
josiashod <djossoujosiashod@gmail.com>
2022-03-23 22:53:16 +0100

    Task 0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string whose length should be return
+ *
+ * Return: (int) the length of the string.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: destination.
+ * @src: source
+ * @n: bytes from src
+ *
+ * Return: pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = _strlen(dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\n'; i++)
+		dest[len + i] = src[i];
+
+	dest[len + i] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+/**
+ * File: main.h
+ * Author: Josias DJOSSOU-DAN
+ *
+ * Description: Header file containing functions declaration
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strncat(char *dest, char *src, int n);
+
+#endif /* MAIN_H */
