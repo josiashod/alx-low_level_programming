8dc31ef922e3dfe5f8697e2610162368f772961f
josiashod <djossoujosiashod@gmail.com>
2022-03-23 23:06:08 +0100

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -31,7 +31,7 @@ char *_strncat(char *dest, char *src, int n)
 	int len = _strlen(dest);
 	int i;
 
-	for (i = 0; i < n && src[i] != '\n'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[len + i] = src[i];
 
 	dest[len + i] = '\0';
