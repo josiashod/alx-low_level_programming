84ed6ee3145f3d8e4bd17d53157dd2b1295f6283
josiashod <djossoujosiashod@gmail.com>
2022-03-24 01:12:42 +0100

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string to convert
+ *
+ * Return: the converted string.
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i++])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') &&
+			(str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ' ' ||
+			 str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			 str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' ||
+			 str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			 str[i - 1] == '}'))
+			str[i] -= 32;
+	};
+
+	return (str);
+}
