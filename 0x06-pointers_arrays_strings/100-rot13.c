df6038766e3df89880dc471f06f70d7016b73245
josiashod <djossoujosiashod@gmail.com>
2022-03-24 11:22:47 +0100

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @str: the string to encode
+ *
+ * Return: the encoded string.
+ */
+char *rot13(char *str)
+{
+	char alpha[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
+			  'H', 'I', 'J', 'K', 'L', 'M', 'N',
+			  'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+			  'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+			  'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			  'j', 'k', 'l', 'm', 'n', 'o', 'p',
+			  'q', 'r', 's', 't', 'u', 'v', 'w',
+			  'x', 'y', 'z'
+	};
+
+	char key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z', 'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J', 'K',
+			'L', 'M', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w',
+			'x', 'y', 'z', 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm'
+	};
+	int i = 0, j;
+
+	while (str[i])
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = key[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
