158dec7531d563efbe3f68dc8bd3431ef8374a5c
josiashod <djossoujosiashod@gmail.com>
2022-03-24 11:32:35 +0100

    Task 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -17,5 +18,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int);
 
 #endif /* MAIN_H */
