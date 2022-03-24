f90df0a66be60a697af1a2de45767c1b4b4dc3d6
josiashod <djossoujosiashod@gmail.com>
2022-03-24 09:19:57 +0100

    Task 6 update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,16 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i++])
+	while (str[++i])
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') &&
-			(str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ' ' ||
-			 str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-			 str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' ||
-			 str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-			 str[i - 1] == '}'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ' ' ||
+		    str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+		    str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+		    str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+		    str[i - 1] == '}')
 			str[i] -= 32;
 	};
 
