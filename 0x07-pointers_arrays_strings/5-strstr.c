5530851b3cbe22203b73ec1a3247826120d2a520
josiashod <djossoujosiashod@gmail.com>
2022-03-28 17:52:01 +0100

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: an integer less than,
+ * equal to, or greater than zero
+ * if s1 is found
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int comp = 0, i = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			comp = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			comp = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			comp = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+
+	}
+
+	return (comp);
+}
+
+/**
+ * _strstr - that locates a substring.
+ * @haystack: the source
+ * @needle: the substring to find
+ *
+ * Return: a pointer to the beginning
+ * of the located substring, or NULL
+ * if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if (_strcmp(haystack, needle) == 0)
+			return (haystack);
+		haystack++;
+	}
+
+	return (NULL);
+}
