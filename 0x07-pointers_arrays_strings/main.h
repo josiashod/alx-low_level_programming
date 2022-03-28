084c7df6421056fa2b8d29695cbd685a0203c259
josiashod <djossoujosiashod@gmail.com>
2022-03-28 15:52:34 +0100

    Task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+/**
+ * File: main.h
+ * Author: Josias DJOSSOU-DAN
+ *
+ * Description: Header file containing functions declaration
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
