e6cc0706078949efa10f30e13713a0a0d54950b4
josiashod <djossoujosiashod@gmail.com>
2022-03-30 12:10:31 +0100

    Task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * File: main.h
+ * Author: Josias DJOSSOU-DAN
+ *
+ * Description: Header file containing functions declaration
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void _puts_recursion(char *);
+
+#endif /* MAIN_H */
