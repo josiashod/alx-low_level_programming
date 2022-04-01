c78b5c6a0349f5db44c0ded08d79121db1ec3ae9
josiashod <djossoujosiashod@gmail.com>
2022-04-01 15:45:00 +0100

    Update control

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,9 +13,9 @@ int main(int argc, char **argv)
 {
 	int i, mul = 1;
 
-	if (argc > 1)
+	if (argc > 2)
 	{
-		for (i = 1; i < argc; i++)
+		for (i = 1; i < 3; i++)
 			mul *= atoi(argv[i]);
 		printf("%d\n", mul);
 		return (0);
