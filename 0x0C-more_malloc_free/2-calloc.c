27d994feaf7f6be563d06591901b3db6e4a3e713
josiashod <djossoujosiashod@gmail.com>
2022-04-07 16:07:46 +0100

    Update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
+	for (i = 0; i < (nmem * size); i++)
+		ptr[i] = 0;
 	return (ptr);
 }
