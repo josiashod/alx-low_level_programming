d80613ef10a5d8b1baa38668b04021b3fcc2e9a2
josiashod <djossoujosiashod@gmail.com>
2022-04-11 15:56:44 +0100

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * dog - dog structure
+ * @name: the dog name
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: This is the structure
+ * of a dog
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
