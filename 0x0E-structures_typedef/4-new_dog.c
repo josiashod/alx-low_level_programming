e8802ab0177bfe48aa2b9f01ed9b1a44fa607cf6
josiashod <djossoujosiashod@gmail.com>
2022-04-11 18:27:31 +0100

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,6 +34,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
 
 	if (!name && !owner && age < 0)
 		return (NULL);
