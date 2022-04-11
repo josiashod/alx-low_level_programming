0cafda7ed60ba9c45de480e7227a563b4718375a
josiashod <djossoujosiashod@gmail.com>
2022-04-11 13:26:01 +0100

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - funtion that the name of the current file
+ *
+ * Return: Always success.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
