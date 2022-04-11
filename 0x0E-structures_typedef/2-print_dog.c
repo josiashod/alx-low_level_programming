53cff46790fb8f9e53ebca1a54d80049634df7ce
josiashod <djossoujosiashod@gmail.com>
2022-04-11 17:59:42 +0100

    push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,20 +7,19 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d)
 	{
-		if ((*d).name != NULL)
+		if ((*d).name)
 			printf("Name: %s\n", (*d).name);
 		else
 			printf("Name: (nil)\n");
-		if ((*d).age != NULL)
+		if ((*d).age)
 			printf("Age: %.6f\n", (*d).age);
 		else
 			printf("Age: (nil)\n");
-		if ((*d).owner != NULL)
+		if ((*d).owner)
 			printf("Owner: %s\n", (*d).owner);
 		else
 			printf("Owner: (nil)\n");
 	}
-	return;
 }
