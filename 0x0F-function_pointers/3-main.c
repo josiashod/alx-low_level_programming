3d24d9a7f5b94cafe2ed0684ee1a88da8225a9fd
josiashod <djossoujosiashod@gmail.com>
2022-04-13 21:21:19 +0100

    adding 3-main.C

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * main - main
+ * @argc: the number of arguments
+ * @argv: array of arguments.
+ * Return: always success
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+	exit(0);
+}
