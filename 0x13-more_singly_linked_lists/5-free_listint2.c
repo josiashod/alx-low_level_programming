2e04135dc7501f6585923760959c478ad851f1a8
josiashod <djossoujosiashod@gmail.com>
2022-04-25 16:39:18 +0100

    Task 5 code review

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *tmp, *list = **head;
 
-	while (*head)
+	while (list)
 	{
-		tmp = *head;
-		*head = (*head)->next;
+		tmp = list;
+		list = list->next;
 		free(tmp);
 	}
 }
