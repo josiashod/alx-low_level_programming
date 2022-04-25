731f6445cbe355a93bd0a5ca4574fad080449d4d
josiashod <djossoujosiashod@gmail.com>
2022-04-25 18:01:10 +0100

    Task 9 new review

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev, *new;
 	unsigned int i = 0;
 
-	while (head)
+	while (head && *head)
 	{
 		if (idx == i)
 		{
