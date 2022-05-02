310da8e71d712b156c50e27b6dd28f47f9309619
josiashod <djossoujosiashod@gmail.com>
2022-05-02 18:56:36 +0100

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * init_buffer - init of the write buffer
+ * @filename: - the name of the file
+ *
+ * Return: pointer to the buffer
+ *
+ */
+char *init_buffer(char *filename)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * _close - close file
+ * @fd: file descriptor
+ *
+ */
+void _close(ssize_t fd)
+{
+	ssize_t cl;
+
+	cl = close(fd);
+
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %li\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: the number of arguments
+ * @argv: array of arguments.
+ *
+ * Return: the exit code.
+ */
+int main(int argc, char **argv)
+{
+	ssize_t file_from, file_to, rd, wr;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = init_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	rd = read(file_from, buffer, 1024);
+
+	do {
+		if (file_from == -1 || rd == -1)
+		{
+			free(buffer);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(97);
+		}
+
+		wr = write(file_to, buffer, 1024);
+		if (wr == -1 || file_to == -1)
+		{
+			free(buffer);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+
+		rd = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (rd > 0);
+
+	free(buffer);
+	_close(file_from);
+	_close(file_to);
+	return (0);
+}
