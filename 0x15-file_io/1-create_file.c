943c55a7b19052b8af67c096e605ff23774e2ad6
josiashod <djossoujosiashod@gmail.com>
2022-05-02 18:11:27 +0100

    handle text_content nll

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,8 +28,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (text_content)
+		len = _strlen(text_content);
 
-	len = _strlen(text_content);
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
 
