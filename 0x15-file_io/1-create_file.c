734e559d1d310da9b19c07ae5549f90bf1c10720
josiashod <djossoujosiashod@gmail.com>
2022-05-02 18:10:02 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	len = _strlen(text_content);
-	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
 
 	if (op == -1 || wr == -1)
