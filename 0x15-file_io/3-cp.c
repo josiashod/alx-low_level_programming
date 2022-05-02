0a0be73bbc98876bfa5fdaef070e5ba0e5bdbd9b
josiashod <djossoujosiashod@gmail.com>
2022-05-02 19:01:37 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,6 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
  * init_buffer - init of the write buffer
