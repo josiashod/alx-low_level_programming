cb715134f0b98a5fc04c9b23a79de001fe4adaaa
josiashod <djossoujosiashod@gmail.com>
2022-06-30 19:16:02 +0100

    Update hash_table_set code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,17 +36,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		while (h_node->next && strcmp(h_node->key, key) != 0)
 			h_node = h_node->next;
 
-		if (!strcmp(h_node->key, key))/* if key exist in list update it */
+		if (!strcmp(h_node->key, key))/* if key exist in list & update it */
 			h_node->value = strdup(value);
 		else /* otherwise add it to the list */
 		{
-			h_node->next = malloc(sizeof(hash_node_t));
-			if (h_node->next == NULL) /* check allocation failed */
-				return (0);
-
-			h_node->next->key = strdup(key);
-			h_node->next->value = strdup(value);
-			h_node->next->next = NULL;
+			h_node= malloc(sizeof(hash_node_t));
+			h_node->key = strdup(key);
+			h_node->value = strdup(value);
+			h_node->next = ht->array[hash_key];
+			ht->array[hash_key] = h_node;
 		}
 	}
 	return (1); /* program ran without any error */
